e52574d2e70873b945d831a4b3dc210b3b64b5d4
ferrisatassi1 <ferris.atassi@gmail.com>
2022-04-11 15:19:30 -0500

    Fixed Conflicts

--- src/BudgetAllocator/AllocMain.java
+++ src/BudgetAllocator/AllocMain.java
@@ -61,32 +61,3 @@ public class AllocMain {
 		
 	}
 }
-package BudgetAllocator;
-
-import java.io.*;
-import java.util.Scanner;
-
-public class AllocMain {
-
-	public static void main(String[] args) {
-		boolean correctFile = false;
-
-		Scanner textScanner = new Scanner(System.in);
-
-		System.out.println("Please input your exactnCSV file name that is in this directory: ");
-		Scanner CSVParser;
-		String fileName = textScanner.nextLine();
-		try {
-			CSVParser = new Scanner(new File("F:\\" + fileName + ".csv"));
-			correctFile = true;
-			CSVParser.useDelimiter(",");
-			while(CSVParser.hasNext()) {
-				System.out.print(CSVParser.next());
-			}
-			CSVParser.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found, please enter again.");
-		}
-
-	}
-}
