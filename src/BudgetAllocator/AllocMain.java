4c3b90ce4854e5575be4cda1753ba83dafd35091
bennoishi <boishi@wustl.edu>
2022-04-26 18:39:47 -0500

    file parsing is now done through a helper method

--- src/BudgetAllocator/AllocMain.java
+++ src/BudgetAllocator/AllocMain.java
@@ -5,19 +5,78 @@ import java.util.*;
 
 public class AllocMain {
 
+	private static int monthDuration = 0;
+	private static String name = "";
+	private static double initialSavings = 0;
+	private static double income = 0;
+	
+	static ArrayList<String> itemNames = new ArrayList<String>();
+	static ArrayList<Double> itemCosts = new ArrayList<Double>();
 	
 	public static void main(String[] args) {
+		
+		Scanner textScanner = new Scanner(System.in);
+		
+		parseFile(textScanner);
+		
+		Budget budget = new Budget(name, initialSavings, monthDuration, income);
+		for (int i = 0; i < itemNames.size(); i++) {
+			budget.addItem(new BudgetItem(itemNames.get(i), itemCosts.get(i)));
+		}
+		Menu menu = new Menu(budget, textScanner);
+
+		boolean userQuit = false;
+		
+		boolean donePicking = false;
+		for (int i = 0; i < monthDuration; i++) {
+			donePicking = false;
+			while (!donePicking) {
+				System.out.println("Would you like to add a budget item or income source? Select 1");
+				System.out.println("Would you like to remove a budget item? Select 2");
+				System.out.println("Would you like to proceed to next month? Select 3");
+				System.out.println("If you would like to quit the program, type 'q'");
+				String testQuit = textScanner.nextLine();
+				if(testQuit.equals("q")) {
+					userQuit = true;
+					break;
+				}
+				int choice = Integer.parseInt(testQuit);
+				//textScanner.nextLine();
+				if (!(choice >= 1 && choice <= 4)) {
+					System.out.println("Invalid Choice: Please enter again.");
+				} else if (choice == 1) { //add budget item
+					System.out.println("Would you like to add an income item or a budget item? Enter 'i' for income or 'b' for budget");
+					char incomeOrBudget = textScanner.next().charAt(0);
+					textScanner.nextLine();
+					while(incomeOrBudget != 'i' && incomeOrBudget != 'b') {
+						System.out.println("Please enter either 'i' for income item or 'b' for budget item");
+						incomeOrBudget = textScanner.next().charAt(0);
+					}
+					if(incomeOrBudget == 'i') { //choice between income or budget
+						menu.addIncomeItem();
+					}else if(incomeOrBudget == 'b'){
+						menu.addBudgetItem();
+					}
+				} else if (choice == 2) { //remove item
+					menu.removeItem();
+				} else { //proceed to next month and print current
+					donePicking = true;
+					menu.printInterface(i);
+				}
+			}
+			if(userQuit) {
+				break;
+			}
+		}
+		System.out.println("Thank you for using our Budget Allocator!");
+	}
+	
+	public static void parseFile(Scanner textScanner) {
 		boolean correctFile = false;
 
-		Scanner textScanner = new Scanner(System.in);
 		Scanner txtParser;
 		int whichLine = 0;
-		int monthDuration = 0;
-		String name = "";
-		double initialSavings = 0;
-		double income = 0 ;
-		ArrayList<String> itemNames = new ArrayList<String>();
-		ArrayList<Double> itemCosts = new ArrayList<Double>();
+		
 		while(!correctFile) {
 			try {
 				
@@ -56,56 +115,6 @@ public class AllocMain {
 				System.out.println("File not found, please enter again.");
 			}
 		}
-		
-		Budget budget = new Budget(name, initialSavings, monthDuration, income);
-		for (int i = 0; i < itemNames.size(); i++) {
-			budget.addItem(new BudgetItem(itemNames.get(i), itemCosts.get(i)));
-		}
-		Menu menu = new Menu(budget, textScanner);
-
-		boolean userQuit = false;
-		
-		boolean donePicking = false;
-		for (int i = 0; i < monthDuration; i++) {
-			donePicking = false;
-			while (!donePicking) {
-				System.out.println("Would you like to add a budget item or income source? Select 1");
-				System.out.println("Would you like to remove a budget item? Select 2");
-				System.out.println("Would you like to proceed to next month? Select 3");
-				System.out.println("If you would like to quit the program, type 'q'");
-				String testQuit = textScanner.nextLine();
-				if(testQuit.equals("q")) {
-					userQuit = true;
-					break;
-				}
-				int choice = Integer.parseInt(testQuit);
-				textScanner.nextLine();
-				if (!(choice >= 1 && choice <= 4)) {
-					System.out.println("Invalid Choice: Please enter again.");
-				} else if (choice == 1) { //add budget item
-					System.out.println("Would you like to add an income item or a budget item? Enter 'i' for income or 'b' for budget");
-					char incomeOrBudget = textScanner.next().charAt(0);
-					textScanner.nextLine();
-					while(incomeOrBudget != 'i' && incomeOrBudget != 'b') {
-						System.out.println("Please enter either 'i' for income item or 'b' for budget item");
-						incomeOrBudget = textScanner.next().charAt(0);
-					}
-					if(incomeOrBudget == 'i') { //choice between income or budget
-						menu.addIncomeItem();
-					}else if(incomeOrBudget == 'b'){
-						menu.addBudgetItem();
-					}
-				} else if (choice == 2) { //remove item
-					menu.removeItem();
-				} else { //proceed to next month and print current
-					donePicking = true;
-					menu.printInterface(i);
-				}
-			}
-			if(userQuit) {
-				break;
-			}
-		}
-		System.out.println("Thank you for using our Budget Allocator!");
 	}
+	
 }
